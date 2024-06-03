ea37b8fd9999a1c5060d82a8a4b32a2266d5bc56
Rameshkumar A <rameshkumar.ayyasamy@mscdirect.com>
2024-06-03 16:27:45 +0530

    async and manual randomized function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const http = require('http');
 const _ = require('lodash');
 
-//Read the colors from color_palette file
-
-fs.readFile('../src/color_palette.json', 'utf8', (err, colors) => {
-  if (err) {
-    console.error('Error if file is not readed:', err);
-    return;
+async function readJsonFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    throw new Error(`Error reading or parsing file: ${err.message}`);
   }
+}
 
-  const colorPalette = JSON.parse(colors);
-
-  // Create an HTTP server
-  const server = http.createServer((req,res) => {
-
-    const randomizedColors = _.sampleSize(colorPalette, 5);
+async function createServer(data){
+   const server = http.createServer((req,res) => {
 
     //Create response header
     res.writeHead(200, { 'Content-Type': 'application/json' });
 
     // Send the randomized colors as JSON to response
-    res.end(JSON.stringify(randomizedColors));  
+    res.end(JSON.stringify(data));  
   });
+  return server;
+}
+function randomize(array,size) {
+  if (array.length < size) {
+    throw new Error('Array length should be equal or gearter that size');
+  }
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array.slice(0, size);
+}
 
-  //Start the server
-  server.listen(3001, () => {
-    console.log('Server running at http://localhost:3001/');
-  });
-});
+(async function randmizeColors(inputFile,numberOfColors){
+  try {
+    const colorPalette = await readJsonFile(inputFile);
+    const randomizedColors = randomize(colorPalette, numberOfColors);
+    const server = await createServer(randomizedColors);
+    server.listen(3009, () => {
+      console.log('Server running at http://localhost:3009/');
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+})('../src/color_palette.json', 5);
