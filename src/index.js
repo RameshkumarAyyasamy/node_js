638d6d3c18c15b8a607ef5e8cd883b598b6f9cdc
Rameshkumar A <rameshkumar.ayyasamy@mscdirect.com>
2024-06-03 15:36:50 +0530

    async approach

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const _ = require('lodash');
 
-//Read the colors from color_palette file
-
-fs.readFile('../src/color_palette.json', 'utf8', (err, colors) => {
-  if (err) {
-    console.error('Error if file is not readed:', err);
-    return;
+//Read the Data from the mentioned file
+async function readJsonFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    //log the error oif the file cannot be read.
+    throw new Error(`Error reading or parsing file: ${err.message}`);
   }
-  const colorPalette = JSON.parse(colors);
-  //Randomize 5 colors
-  const randomizedColors = _.sampleSize(colorPalette, 5);
+}
 
-  //Write the randomized colors in randomized_palette.json file
-  fs.writeFile('randomized_color_palette.json', JSON.stringify(randomizedColors), (err) => {
-    if (err) {
-      console.error('Error writing file:', err);
-      return;
-    }
+//Write the data to the required file/ create a file if it's not exist
+async function writeJsonFile(filePath, data) {
+  try {
+    await fs.writeFile(filePath, JSON.stringify(data), 'utf8');
+  } catch (err) {
+    //log the error oif the file cannot be create/write.
+    throw new Error(`Error writing file: ${err.message}`);
+  }
+}
 
-    //Read the randomized colors from randomized_color_palette file
-    fs.readFile('randomized_color_palette.json', 'utf8', (err, colors) => {
-      if (err) {
-        console.error('Error if file is not readed:', err);
-        return;
-      }
-      const randomizedColorPalette = JSON.parse(colors);
+//Randomizing colors
+async function randomizeColors(inputFile, outputFile, numberOfColors) {
+  try {
+    //Read the color palette
+    const colorPalette = await readJsonFile(inputFile);
+    //Randomize the colors
+    const randomizedColors = _.sampleSize(colorPalette, numberOfColors);
+    //write the randomized colors in a new file
+    await writeJsonFile(outputFile, randomizedColors);
+    //Read the randomized colors
+    const readRandomizedColors = await readJsonFile(outputFile);
+    //Print the randomied colors
+    readRandomizedColors.forEach(color => console.log(color));
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
-      //Printing 5 randomized colors in console
-      randomizedColorPalette.forEach(color => console.log(color));
-    });
-  });
-});
+(async () => {
+  const inputFilePath = '../src/color_palette.json';
+  const outputFilePath = 'randomized_color_palette.json';
+  
+  await randomizeColors(inputFilePath, outputFilePath, 5);
+})();
